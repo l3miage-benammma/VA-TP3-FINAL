c3a88d0c3d87fa75260f4d5b504f468210a3374f
l3miage-benammma <mahmoud.benammar12@gmail.com>
2024-04-07 00:49:57 +0200

    creation d'une session faite

---
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/component/ExamComponentTest.java
@@ -0,0 +1,48 @@
+package fr.uga.l3miage.spring.tp3.component;
+
+import fr.uga.l3miage.spring.tp3.components.ExamComponent;
+import fr.uga.l3miage.spring.tp3.exceptions.technical.ExamNotFoundException;
+import fr.uga.l3miage.spring.tp3.models.ExamEntity;
+import fr.uga.l3miage.spring.tp3.repositories.ExamRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+
+public class ExamComponentTest {
+    @Autowired
+    private ExamComponent examComponent;
+
+    @MockBean
+    private ExamRepository examRepository;
+
+    @Test
+    void testGetExamEntityByIdNotFound(){
+        when(examRepository.findById(anyLong())).thenReturn(Optional.empty());
+        assertThrows(ExamNotFoundException.class,()->examComponent.getAllById(Set.of(anyLong())));
+    }
+
+    @Test
+    void testGetExamEntityByIdFound(){
+        ExamEntity examEntity = ExamEntity.builder()
+                .id(1L)
+                .weight(1)
+                .name("IHM")
+                .build();
+        when(examRepository.findAllById(anySet())).thenReturn(List.of(examEntity));
+        assertDoesNotThrow(()->examComponent.getAllById(Set.of(anyLong())));
+    }
+}
---
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/services/SessionServiceTest.java
@@ -0,0 +1,34 @@
+package fr.uga.l3miage.spring.tp3.services;
+
+import fr.uga.l3miage.spring.tp3.components.SessionComponent;
+import fr.uga.l3miage.spring.tp3.mappers.SessionMapper;
+import fr.uga.l3miage.spring.tp3.repositories.EcosSessionProgrammationRepository;
+import fr.uga.l3miage.spring.tp3.repositories.EcosSessionProgrammationStepRepository;
+import fr.uga.l3miage.spring.tp3.repositories.EcosSessionRepository;
+import fr.uga.l3miage.spring.tp3.request.SessionCreationRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class SessionServiceTest {
+    @Autowired
+    private SessionComponent sessionComponent;
+    @MockBean
+    private EcosSessionRepository ecosSessionRepository;
+    @MockBean
+    private EcosSessionProgrammationRepository ecosSessionProgrammationRepository;
+    @MockBean
+    private EcosSessionProgrammationStepRepository ecosSessionProgrammationStepRepository;
+    @SpyBean
+    private SessionMapper sessionMapper;
+
+
+
+
+
+}
